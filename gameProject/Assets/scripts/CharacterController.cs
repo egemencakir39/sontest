66134e306b1231c0e617d3da655bd41bbca697b2
egemencakir39 <egemencakir29@gmail.com>
2023-10-26 17:41:08 +0300

    Dash (animasyonu ayarlanmamış hal)

--- gameProject/Assets/scripts/CharacterController.cs
+++ gameProject/Assets/scripts/CharacterController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 10f;
+    public float moveSpeed = 5f; // hareket hýzý
+    public float jumpForce = 10f; // zýplama gücü
     public int extraJumps = 1; // Çift zýplama hakký
-    private int remainingJumps;
+    public float dashingPower = 24f; // dash gücü
+    public float dashingTime = 0.2f; // dash süresi
+    public float dashingCoolDown = 1f; 
+    private int remainingJumps; // tekrarlanabilir zýplama
     private bool isGrounded = false;
     private Rigidbody2D rb;
     private Collider2D col;
-    
+    private bool canDash = true;
+    private bool isDashing;
+    [SerializeField] private TrailRenderer tr;
     
     
 
@@ -25,6 +30,7 @@ public class CharacterController : MonoBehaviour
 
     private void Update()
     {
+       
         flipface();
         // zemin temas kontrolü
         isGrounded = Physics2D.IsTouchingLayers(col, LayerMask.GetMask("Ground"));
@@ -40,10 +46,18 @@ public class CharacterController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             remainingJumps--;
         }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) //dash tuþ kontrolü
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
         // Karakter hareketi
         float horizontalInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
@@ -62,5 +76,20 @@ public class CharacterController : MonoBehaviour
             GeciciScale.x = -10f;
         }
         transform.localScale = GeciciScale;
+    }
+    private IEnumerator Dash() //dash donksiyonu
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCoolDown);
+        canDash = true;
     }
 }
