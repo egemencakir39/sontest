e5b985c14ba912221d9411678fce47c711b8f85b
egemencakir39 <egemencakir29@gmail.com>
2023-12-05 23:33:44 +0300

    Grappling hook mekaniği bug fix

--- gameProject/Assets/scripts/GraplingHook.cs
+++ gameProject/Assets/scripts/GraplingHook.cs
@@ -4,55 +4,64 @@ using UnityEngine;
 
 public class GraplingHook : MonoBehaviour
 {
-    [SerializeField] private Transform Hedefpos; 
-    [SerializeField] private LineRenderer lr; 
-    [SerializeField] private DistanceJoint2D dj; 
+    [SerializeField] private Transform Hedefpos;
+    [SerializeField] private LineRenderer lr;
+    [SerializeField] private DistanceJoint2D dj;
     [SerializeField] private float activationDistance = 5f;
-    [SerializeField] private Rigidbody2D rb;
 
-    // Start is called before the first frame update
+    private bool isHookActive = false;
+
     void Start()
     {
-        dj.enabled = false; // dj baþlangýçta devre dýþý býrakýlýr
+        dj.enabled = false;
+        lr.positionCount = 0; // Oyun baþladýðýnda çizgi çekili deðil
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // q tuþuna basýldýðýnda devreye girecek
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q) && IsWithinActivationDistance())
         {
-            if (IsWithinActivationDistance())
-            {
-                HookOn(); // Hook'u etkinleþtir
-            }
+            ToggleHook(); //kanca aç kapa
         }
-        else if (Input.GetKeyUp(KeyCode.Q))
+
+        if (isHookActive)
         {
-            HookOff(); // q tuþu býrakýldýðýnda hooku devre dýþý býrak
+            lr.SetPosition(1, transform.position); //kanca çizgisi
         }
     }
 
-    // activationdistance içindemi
-    private bool IsWithinActivationDistance()
+    private bool IsWithinActivationDistance() //aktivasyon mesafe kontrolü
     {
         return Vector2.Distance(transform.position, Hedefpos.position) <= activationDistance;
     }
 
-    // Hook'u etkinleþtir
-    public void HookOn()
+    private void ToggleHook()
     {
-        dj.enabled = true; // dj etkinleþtir
-        dj.connectedAnchor = Hedefpos.position; // Baðlantý noktasýný ayarla
-        lr.positionCount = 2; // LineRenderer pozisyon sayýsýný ayarla
-        lr.SetPosition(0, Hedefpos.position); // LineRenderer'ýn baþlangýç pozisyonunu ayarla
-        lr.SetPosition(1, transform.position); // LineRenderer'ýn bitiþ pozisyonunu ayarla
+        isHookActive = !isHookActive;
+
+        if (isHookActive)
+        {
+            HookOn(); //hook açýlýyorsa bu fonksiyonu çaðýr
+        }
+        else
+        {
+            HookOff(); //hook kapancaksa bunu çaðýr
+        }
     }
 
-    // Hook'u devre dýþý býrak
+    public void HookOn()
+    {
+        dj.enabled = true;
+        dj.connectedAnchor = Hedefpos.position;
+
+        lr.positionCount = 2;
+        lr.SetPosition(0, Hedefpos.position);
+        lr.SetPosition(1, transform.position);
+    }
+
     public void HookOff()
     {
-        dj.enabled = false; // DistanceJoint2D'yi devre dýþý býrak
-        lr.positionCount = 0; // LineRenderer pozisyon sayýsýný sýfýrla
+        dj.enabled = false;
+        lr.positionCount = 0;
     }
 }
