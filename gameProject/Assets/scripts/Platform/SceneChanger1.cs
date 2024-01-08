4941a5c49a8421cebaa96cc53f49049610640302
egemencakir39 <egemencakir29@gmail.com>
2024-01-08 14:16:44 +0300

    anahtar mekaniği eklendi

--- gameProject/Assets/scripts/Platform/SceneChanger1.cs
+++ gameProject/Assets/scripts/Platform/SceneChanger1.cs
@@ -5,14 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger1 : MonoBehaviour
 {
+    private int collectedKeys = 0;
+    [SerializeField] private int requiredKeys = 5;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("kapý"))
+        if (collision.gameObject.CompareTag("kapý") && collectedKeys >= requiredKeys)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
-
+        if (collision.gameObject.CompareTag("anahtar"))
+        {
+            collectKey();
+            Destroy(collision.gameObject);
+            Debug.Log("alýndý anahtar");
+        }
+    }
+   
+    public void collectKey()
+    {
+        collectedKeys++;
+        if (collectedKeys >= requiredKeys)
+        {
+            Debug.Log("kapý açýlabilir");
+        }
     }
 }
 
-   
+
