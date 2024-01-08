cec3bddc0337c7ac873ce69042c5534db2432fb6
egemencakir39 <egemencakir29@gmail.com>
2024-01-09 00:15:55 +0300

    key ekrana yazdırıldı

--- gameProject/Assets/scripts/Platform/SceneChanger1.cs
+++ gameProject/Assets/scripts/Platform/SceneChanger1.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChanger1 : MonoBehaviour
 {
     private int collectedKeys = 0;
     [SerializeField] private int requiredKeys = 5;
+    [SerializeField] private TMP_Text keyCountText;
+
+    private void Update()
+    {
+        keyCountText.text = collectedKeys.ToString();
+    }
+    private void Start()
+    {
+       
+        collectedKeys = 0;
+        keyCountText.text = collectedKeys.ToString();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("kapý") && collectedKeys >= requiredKeys)
@@ -18,9 +35,11 @@ public class SceneChanger1 : MonoBehaviour
             collectKey();
             Destroy(collision.gameObject);
             Debug.Log("alýndý anahtar");
+           
+            Debug.Log("Key Count: " + collectedKeys);
         }
     }
-   
+
     public void collectKey()
     {
         collectedKeys++;
@@ -29,6 +48,8 @@ public class SceneChanger1 : MonoBehaviour
             Debug.Log("kapý açýlabilir");
         }
     }
+
+    
 }
 
 
