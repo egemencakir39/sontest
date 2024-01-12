0993f8a9d837b2dff58c5e30ef6d5bd9ea82cb98
egemencakir39 <egemencakir29@gmail.com>
2024-01-12 22:38:19 +0300

    sprite kalite + uzak enemy

--- gameProject/Assets/scripts/Enemy/enemyRun.cs
+++ gameProject/Assets/scripts/Enemy/enemyRun.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class enemyRun : MonoBehaviour
 {
     public float speed = 5f; // Koþma hýzý
-    public float detectionRange = 5f; // Oyuncuyu algýlama mesafesi
+    public float detectionRangeX = 8f; // Sadece x ekseni üzerindeki algýlama mesafesi
+    public float detectionRangeY = 2f; // Sadece y ekseni üzerindeki algýlama mesafesi
     private Transform player; // Oyuncu konumu
 
     void Start()
@@ -15,17 +16,21 @@ public class enemyRun : MonoBehaviour
 
     void Update()
     {
-        // Oyuncuya belirli bir mesafeden yaklaþýldýðýnda
-        if (Vector2.Distance(transform.position, player.position) < detectionRange)
+        // Oyuncuya belirli bir mesafeden x ve y ekseni üzerinde yaklaþýldýðýnda
+        float distanceToPlayerX = Mathf.Abs(transform.position.x - player.position.x);
+        float distanceToPlayerY = Mathf.Abs(transform.position.y - player.position.y);
+
+        if (distanceToPlayerX < detectionRangeX && distanceToPlayerY < detectionRangeY)
         {
             // Oyuncuya doðru koþ
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            float moveDirection = Mathf.Sign(player.position.x - transform.position.x);
+            transform.Translate(Vector2.right * moveDirection * speed * Time.deltaTime);
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        // Oyuncuya deðildiðinde silin
+        
         if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
