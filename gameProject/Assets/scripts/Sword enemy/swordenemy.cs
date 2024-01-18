b3f457184d3b6784595e33368c004f9ac595dd07
egemencakir39 <egemencakir29@gmail.com>
2024-01-18 12:07:31 +0300

    sword enemy eklendi

---
+++ gameProject/Assets/scripts/Sword enemy/swordenemy.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swordenemy : MonoBehaviour
+{
+    soundManager SoundManagerScript;
+    public Transform pointA;
+    public Transform pointB;
+    public float moveSpeed = 5f;
+    public int Damage = 10;
+    private Animator animator;
+    private bool movingToA = true;
+
+    // Player takip özellikleri
+    public float chaseSpeed = 5f;
+    public float chaseDistance = 10f;
+    private Transform player;
+
+    // Atak özellikleri
+    public float attackDistance = 5f; // Atak mesafesi
+    public float timeBetweenAttacks = 2f; // Ataklar arasýndaki süre
+    private float attackTimer;
+    private bool isWalking = false;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+        SoundManagerScript = GameObject.Find("SoundManager").GetComponent<soundManager>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+            // Oyuncuya yaklaþýldýðýnda
+            if (distanceToPlayer < chaseDistance)
+            {
+                // player takip sadece x ekseni
+                Vector3 targetPosition = new Vector3(player.position.x, transform.position.y, player.position.z);
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, chaseSpeed * Time.deltaTime);
+                isWalking = true;
+
+                FlipTowardsPlayer(player.position.x);
+
+                // atak mesafesi
+                if (distanceToPlayer < attackDistance)
+                {
+                    // atak timer
+                    if (Time.time > attackTimer)
+                    {
+                        // random atak
+                        int randomAttack = Random.Range(1, 3); // 1 veya 2
+                        if (randomAttack == 1)
+                        {
+                            Attack1();
+
+                        }
+                        else
+                        {
+                            Attack2();
+
+                        }
+
+                        // timer güncelle
+                        attackTimer = Time.time + timeBetweenAttacks;
+                    }
+                }
+            }
+            else
+            {
+                if (movingToA)
+                {
+                    if (Vector2.Distance(transform.position, pointA.position) > 0.1f)
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, pointA.position, moveSpeed * Time.deltaTime);
+                        FlipTowardsDirection(true); // Saða bakýyor
+                    }
+                    else
+                    {
+                        movingToA = false;
+                    }
+                }
+                else
+                {
+                    if (Vector2.Distance(transform.position, pointB.position) > 0.1f)
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, pointB.position, moveSpeed * Time.deltaTime);
+                        FlipTowardsDirection(false); // Sola bakýyor
+                    }
+                    else
+                    {
+                        movingToA = true;
+                    }
+                }
+
+                isWalking = Vector2.Distance(transform.position, pointA.position) > 0.1f || Vector2.Distance(transform.position, pointB.position) > 0.1f;
+            }
+        }
+        else
+        {
+            isWalking = false; // Eðer player null ise, isWalking false yap
+        }
+
+        animator.SetBool("walk", isWalking);
+    }
+
+    void Attack1()
+    {
+
+        animator.SetTrigger("attack1");
+        Debug.Log("Attack 1!");
+        FlipTowardsPlayer(player.position.x);
+        chaseSpeed = 1;
+        moveSpeed = 1;
+        Invoke("RestartSpeed", .3f);
+    }
+
+    void Attack2()
+    {
+        chaseSpeed = 1;
+        moveSpeed = 1;
+        animator.SetTrigger("attack1");
+        Debug.Log("Attack 2!");
+        FlipTowardsPlayer(player.position.x);
+        Invoke("RestartSpeed", .3f);
+    }
+
+
+    void RestartSpeed()
+    {
+        // Orijinal hýz deðerlerine geri dön
+        chaseSpeed = 3f;
+        moveSpeed = 3f;
+    }
+    void FlipTowardsPlayer(float playerX)
+    {
+        // Yürüyüþ yönüne göre dönüþ
+        if (transform.position.x < playerX)
+        {
+            transform.localScale = new Vector3(2, 2, 2); // Normal yönde
+        }
+        else
+        {
+            transform.localScale = new Vector3(-2, 2, 2); // Ters yönde
+        }
+    }
+
+    void FlipTowardsDirection(bool facingRight)
+    {
+        // Yürüyüþ yönüne göre dönüþ
+        if (facingRight)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); // Normal yönde
+        }
+        else
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); // Ters yönde
+        }
+    }
+    public void PlayerDead()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
